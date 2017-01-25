58320be7cbcdbeeb58f6bf33eef4ce70c9300f4c
abdulazeeznaji <abdulazeeznaji@yahoo.com>
2017-01-25 09:17:47 +0200

    toy

---
+++ spiralTraversal/spiralTraversal.js
@@ -0,0 +1,24 @@
+/*
+ * Write a function that accepts a 2-dimensional array (array of arrays),
+ * and prints out every value found, but in a spiral from the upper left in to the center
+ * Example:
+    spiralTraversal([
+      [1,2,3],
+      [4,5,6],
+      [7,8,9]
+    ]);
+    returns [1, 2, 3, 6, 9, 8, 7, 4, 5]
+ */
+
+
+
+var spiralTraversal = function(matrix){  
+ for(var i=0; i<matrix.length; i++){
+ 	for(var j=0; j<matrix[i].length; j++){
+ 	
+ 	}
+  }
+};
+//i take thee top part and append it to newArr then
+ // i transpose the remaining matrix after the top after that reverse the transpose
+ //matrix so that when i take the top i actualy take the bottom half..
